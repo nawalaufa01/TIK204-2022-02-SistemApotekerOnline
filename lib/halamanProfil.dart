f464cd71e9615ffe1fa119beacf27876eb22b57b
rezeki <rezeki156@gmail.com>
2022-05-08 23:03:31 +0700

    halaman profil
    berisi tentang profil user

---
+++ lib/halamanProfil.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class HalamanProfil extends StatelessWidget {
+  const HalamanProfil({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'PASIEN',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    size: 34,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30, bottom: 20),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  width: double.infinity,
+                  color: Colors.blueGrey.shade300,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nama               : Yasmina Elma\nUmur                : 20 Tahun\nTanggal lahir   : 06 Okt 2001\nGol Darah         : A+\nJenis Kelamin : Perempuan',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Lokasi Anda',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 15, bottom: 50),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  color: Colors.grey.shade400,
+                  child: Text(
+                    'LR. T Abdullah No A2 Baet, Aceh Besar',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  width: double.infinity,
+                  color: Colors.grey.shade400,
+                  child: Text(
+                    'TAMBAH AKUN',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  width: double.infinity,
+                  color: Colors.grey.shade400,
+                  child: Text(
+                    'TAMBAH AKUN',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
